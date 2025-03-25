46ee118cf5045aa852dbfd6c4dceec16c30a9fb8
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-03-25 15:27:59 +0900

    폴더링

---
+++ ConsoleGameProject/MapData.cpp
@@ -0,0 +1,27 @@
+﻿#include "MapData.h"
+
+namespace Map {
+	// 1. Menu Scene
+	
+
+	// 2. Floor1 Scene
+	
+
+	// 3. Floor2 Scene
+
+
+	// 4. Floor3 Scene
+
+
+	// 5. Floor4 Scene
+
+
+	// 6. Floor5 Scene
+
+
+	// 7. Floor6 Scene
+
+
+	// 8. Floor7 Scene
+
+}
