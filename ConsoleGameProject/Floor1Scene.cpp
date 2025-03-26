cd4e388fd6de0f00d64376404e8537d362e6d9d2
woojeong <77192050+wooj22@users.noreply.github.com>
2025-03-27 00:36:02 +0900

    Feat | 중력, 점프

--- ConsoleGameProject/Floor1Scene.cpp
+++ ConsoleGameProject/Floor1Scene.cpp
@@ -8,7 +8,8 @@
 #include "EndScene.h"
 
 namespace Floor1 {
-	// Map Data
+
+	/*--------- Map Data ---------*/
 	const wchar_t* floor1StaticMap[] = {
 
 	L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓",
@@ -71,20 +72,84 @@ namespace Floor1 {
 	L"▓                                                          ▓",
 	L"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓"
 	};
-    const int mapHeight = sizeof(floor1StaticMap) / sizeof(floor1StaticMap[0]);
+	const int mapHeight = sizeof(floor1StaticMap) / sizeof(floor1StaticMap[0]);
 
-    // Player
+	/*--------- Player ---------*/
 	COORD g_Player = { 3, 57 };
+	bool isJumping = false;
+	float playerVelocityY = 0.0f;
+	float gravity = 300.0f; // 중력
+	float jumpPower = -6.0f; // 점프 속도
 
+	// Move
+	void PlayerMove()
+	{
+		// 임시 좌표
+		int nextX = g_Player.X;
+		int nextY = g_Player.Y;
+
+		// 입력 처리
+		if (Input::IsKeyDown(VK_LEFT) && floor1StaticMap[g_Player.Y][g_Player.X - 1] != L'▓') {
+			nextX--;
+		}
+		if (Input::IsKeyDown(VK_RIGHT) && floor1StaticMap[g_Player.Y][g_Player.X + 1] != L'▓') {
+			nextX++;
+		}
+
+		// 경계 검사 (콘솔 창 크기 기준)
+		if (nextY < 0) nextY = 0;
+
+		// 최종 위치 업데이트
+		g_Player.X = nextX;
+	}
+
+	// Jump
+	void PlayerJump() {
+		float deltaTime = Time::GetElapsedTime();
+
+		// 점프
+		if (Input::IsKeyPressed(VK_UP) && !isJumping)
+		{
+			isJumping = true;
+			playerVelocityY = jumpPower;
+		}
+
+		// 중력 적용
+		if (isJumping)
+		{
+			playerVelocityY += gravity * deltaTime;
+			float nextY = g_Player.Y + playerVelocityY * deltaTime;
+
+			// 바닥 충돌
+			if (floor1StaticMap[(int)nextY + 1][g_Player.X] == L'▓')
+			{
+				isJumping = false;
+				playerVelocityY = 0;
+				g_Player.Y = (int)nextY;
+			}
+			// 아래로 떨어지기
+			else
+			{
+				g_Player.Y = (int)nextY;
+			}
+		}
+	}
+
+	/*--------- Floor1 Game -------------*/
 	// Start
 	void Initalize() {
-
+		Time::Initialize();
 	}
 
 	// Update
 	void Update() {
 		// input & play controll
-		PlayerMove();
+		Time::UpdateTime();
+		if (Time::GetTotalTime() >= 0.1f) {
+			PlayerMove();
+			Time::Initialize();
+		}
+		PlayerJump();
 
 		// scene change
 		if (Input::IsKeyPressed(VK_SPACE)) {
@@ -104,36 +169,5 @@ namespace Floor1 {
 		// player
 		ConsoleRenderer::ScreenDrawChar(g_Player.X, g_Player.Y, 'P', FG_RED);
 	}
-
-	// Player Move
-	void PlayerMove()
-	{
-		// 임시 좌표
-		int nextX = g_Player.X;
-		int nextY = g_Player.Y;
-
-		// 입력 처리
-		if (Input::IsKeyDown(VK_LEFT) && floor1StaticMap[g_Player.Y][g_Player.X - 1] != L'▓') {
-			nextX--;
-		}
-		if (Input::IsKeyDown(VK_RIGHT) && floor1StaticMap[g_Player.Y][g_Player.X + 1] != L'▓') {
-			nextX++;
-		}
-		if (Input::IsKeyDown(VK_UP) && floor1StaticMap[g_Player.Y - 1][g_Player.X] != L'▓') {
-			nextY--;
-		}
-		if (Input::IsKeyDown(VK_DOWN) && floor1StaticMap[g_Player.Y + 1][g_Player.X] != L'▓') {
-			nextY++;
-		}
-
-		// 경계 검사 (콘솔 창 크기 기준)
-		if (nextX < 0) nextX = 0;
-		if (nextX >= ConsoleRenderer::ScreenWidth()) nextX = ConsoleRenderer::ScreenWidth() - 1;
-		if (nextY < 0) nextY = 0;
-		if (nextY >= mapHeight) nextY = mapHeight - 1;
-
-		// 최종 위치 업데이트
-		g_Player.X = nextX;
-		g_Player.Y = nextY;
-	}
 }
+
